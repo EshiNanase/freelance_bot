f22f19c4f9d21fbebf4ca838005e6f3265f680ed
Nanashi <alksndr.zln@gmail.com>
2023-02-22 17:49:57 +0300

    Created serializer for Tariff and added description field

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,28 @@
 from rest_framework import serializers
-from telegram_bot.models import Client, Order
+from telegram_bot.models import Client, Order, Freelancer, Tariff
+
+
+class TariffSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tariff
+        fields = '__all__'
 
 
 class ClientSerializer(serializers.ModelSerializer):
 
-    tariff = serializers.CharField()
+    tariff = TariffSerializer()
 
     class Meta:
         model = Client
         fields = ['chat_id', 'tariff']
+
+
+class FreelancerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Freelancer
+        fields = '__all__'
 
 
 class OrderSerializer(serializers.ModelSerializer):
